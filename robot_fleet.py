a833996c7f5a2a11084e55b4575ced2e405f1f9f
s2202436-spec <s2202436@usls.edu.ph>
2026-08-14 14:03:54 +0800

    Add Robot base class with battery property

---
+++ robot_fleet.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+
+class Robot(ABC):
+    manufacturer = "RoboTech"
+    population = 0
+
+    def __init__(self, name, battery=100):
+        self.name = name
+        self._battery = 100
+        self.battery = battery
+        Robot.population += 1
+
+    @property
+    def battery(self):
+        return self._battery
+
+    @battery.setter
+    def battery(self, value):
+        self._battery = max(0, min(100, value))
+
+    def __str__(self):
+        return f"{self.name} ({self.battery}% battery)"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name='{self.name}', battery={self.battery})"
+
+    @abstractmethod
+    def perform_task(self):
+        pass
